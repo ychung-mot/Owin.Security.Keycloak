dc95c28412e11b0a92895bf10845bddfb0090fd2
Matt Morgan <mattmorg55@gmail.com>
2017-07-20 14:18:53 -0400

    Adding common assembly info

---
+++ src/Build/CommonAssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Microsoft.Owin 3.1 Authentication Middleware for JBoss Keycloak 3.2")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
