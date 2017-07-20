6f5c2a54b25ea0eefd7d28697693d847b63e4f44
Matt Morgan <mattmorg55@gmail.com>
2017-07-20 13:50:12 -0400

    Updating assembly versioning and NuGet package. Changing to one NuGet package for both assemblies.

--- src/Owin.Security.Keycloak/Properties/AssemblyInfo.cs
+++ src/Owin.Security.Keycloak/Properties/AssemblyInfo.cs
@@ -1,37 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("Keycloak Authentication [ASP.NET 4]")]
-[assembly: AssemblyDescription("ASP.NET 4 VERSION - JBoss Keycloak Authentication Middleware for ASP.NET 4 OWIN")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Redhat")]
-[assembly: AssemblyProduct("JBoss Keycloak")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
+[assembly: AssemblyTitle("Owin.Security.Keycloak")]
 [assembly: Guid("fcac3105-3e81-49c4-9d8e-0fc9e608e87c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0.0")]
